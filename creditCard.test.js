5072cbe53f66d33196cbb30534b793179b48430f
Axandria Hamilton <amhamilton1@student.fullsail.edu>
2024-02-10 12:20:54 -0500

    added test

--- creditCard.test.js
+++ creditCard.test.js
@@ -11,6 +11,11 @@ describe('creditCard function', () => {
     expect(result).toBe("Invalid Credit Card");
   });
 
+  test('should return "Invalid Credit Card" for invalid credit card length', () => {
+    const result = creditCard("12345678901234567890");
+    expect(result).toBe("Invalid Credit Card");
+  });
+
   test('should return "Invalid Credit Card" for an empty string', () => {
     const result = creditCard("");
     expect(result).toBe("Invalid Credit Card");
